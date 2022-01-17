c04c35b055d3cbac3ee6a7ed558bd349368a0f6e
Mohammad Maswadeh <mohammad.maswadeh@pwc.com>
2022-01-17 09:11:37 +0200

    Init Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+
+    const url = 'http://api.weatherstack.com/current?access_key=8a303d377d825afdc90b0399f7f06624&query=' + latitude + ',' + longitude + '&units=f'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out.")
+        }
+    })
+}
+
+module.exports = forecast
